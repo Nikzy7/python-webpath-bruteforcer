dc8b8415b824b5f4a20ae582a945ab67d0bd7ed8
Apoorv Negi <43514386+Nikzy7@users.noreply.github.com>
2020-12-14 17:41:13 +0530

    add boilerplate code

---
+++ app.py
@@ -0,0 +1,29 @@
+# website brute force attacker
+import requests
+
+# placeholder variable declaration
+inputSite = str()
+wordFile = str()
+wordList = list()
+statusCodeDictionary = dict()
+
+def url_creator(inputSite,word):
+    pass
+
+# possible multithread function
+def bruteForceAction():
+    pass
+
+def inputProcessing():
+    pass
+
+def actionHandler():
+    pass
+
+def outputGeneration():
+    pass
+
+if __name__ == "__main__":
+    inputProcessing()
+    actionHandler()
+    outputGeneration()
