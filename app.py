11102e1dc508926a0a685e14fbc5848e7bffb163
Apoorv Negi <43514386+Nikzy7@users.noreply.github.com>
2020-12-14 18:10:50 +0530

    add input processing from CLI

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 # website brute force attacker
 import requests
 import sys
+import os
 
 # placeholder variable declaration
-inputSite = str()
+inputURL = str()
 wordFile = str()
 wordList = list()
+errorCodes = list()
 statusCodeDictionary = dict()
 
 # function to generate url from input and word
@@ -18,7 +20,33 @@ def bruteForceAction():
 
 # processing and filling variables
 def inputProcessing():
-    pass
+    try:
+        global inputURL
+        global wordFile
+        global errorCodes
+        global wordList
+
+        inputURL = sys.argv[1]  
+        wordFile = sys.argv[2]
+        
+        for x in range(3,len(sys.argv)):
+            errorCodes.append(int(sys.argv[x]))
+
+        if not(os.path.isfile(wordFile)):
+            raise Exception("The word file does not exist !") 
+
+        wordList = open(wordFile,'r').readlines()
+
+        for x in range(len(wordList)):
+            wordList[x] = wordList[x].strip()
+
+    except IndexError:
+        print("Command Line Arguments have not been properly provided ! Please try again.")
+        sys.exit()
+    except Exception as e:
+        print(e)
+        sys.exit()
+
 
 # handling main action
 def actionHandler():
@@ -28,7 +56,15 @@ def actionHandler():
 def outputGeneration():
     pass
 
+# process check for inputProcessing()
+# def unitTest():
+#     print(errorCodes)
+#     print(wordFile)
+#     print(inputURL)
+#     print(sys.argv)
+
 if __name__ == "__main__":
     inputProcessing()
+    # unitTest()
     actionHandler()
     outputGeneration()
---
+++ test.py
@@ -0,0 +1,5 @@
+import sys
+
+print("a")
+sys.exit()
+print("b")
